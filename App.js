a8b5bebe6b21f5b567409234abd82b1cee52658d
Kaitlyn Ly <kaitlyn.ly@infusionsoft.com>
2018-07-27 22:09:42 -0700

    render views

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
-import { View, Text, ScrollView } from 'react-native';
-import React, { Component } from 'react';
-import globalStyles from './src/styles';
+import { View, Text, ScrollView } from "react-native";
+import React, { Component, Fragment } from "react";
+import globalStyles from "./src/styles";
 
-import SimpleTextComponent from './src/components/SimpleTextComponent';
+import SimpleTextComponent from "./src/components/SimpleTextComponent";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeView: "homeScreen"
+    };
+  }
+
+  renderActiveView = () => {
+    switch (this.state.activeView) {
+      case "homeScreen":
+        return (
+          <Fragment>
+            <Text style={[globalStyles.textInput]}>Hello</Text>
+            <SimpleTextComponent name="Kaitlyn" />
+          </Fragment>
+        );
+      default:
+        return (
+          <View>
+            <Text>Go home</Text>
+          </View>
+        );
+    }
+  };
+
   render() {
     return (
       <View style={globalStyles.screenContainer}>
-        <View style={globalStyles.container}>
-          <Text style={[globalStyles.textInput]}>
-            Hello
-          </Text>
-          <SimpleTextComponent name="Kaitlyn" />
-        </View>
+        <View style={globalStyles.container}>{this.renderActiveView()}</View>
         <View style={globalStyles.navContainer} />
       </View>
     );
