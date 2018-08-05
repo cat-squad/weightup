3ec22aec91c75ce4b8e982315b4925cfdf189ffc
Kaitlyn Ly <kaitlyn.ly@infusionsoft.com>
2018-08-05 09:30:23 -0700

    render list view

--- src/screens/SelectExerciseScreen.js
+++ src/screens/SelectExerciseScreen.js
@@ -8,211 +8,97 @@ import {
   Left,
   Right,
   Title,
-  Body
+  Body,
+  ListItem,
+  CheckBox,
+  Content
 } from "native-base";
 
 export default class SelectExerciseScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      muscleGroups: [
+        {
+          name: "Biceps",
+          selected: false
+        },
+        {
+          name: "Triceps",
+          selected: false
+        },
+        {
+          name: "Shoulders",
+          selected: false
+        },
+        {
+          name: "Back",
+          selected: false
+        },
+        {
+          name: "Abs",
+          selected: false
+        },
+        {
+          name: "Hamstrings",
+          selected: false
+        },
+        {
+          name: "Quads",
+          selected: false
+        },
+        {
+          name: "Calves",
+          selected: false
+        },
+        {
+          name: "Glutes",
+          selected: false
+        },
+        {
+          name: "Chest",
+          selected: false
+        }
+      ]
+    };
+  }
+
   handleSelectedExerciseButtonClick = (nextScreen, selectedMuscleGroup) => {
     this.props.callback_setActiveView(nextScreen);
     this.props.callback_setSelectedMuscleGroup(selectedMuscleGroup);
   };
 
+  renderSelectList = list => {
+    return list.map(item => {
+      return (
+        <ListItem style={{ width: "100%" }}>
+          <CheckBox checked={item.selected} />
+          <Body>
+            <Text>{item.name}</Text>
+          </Body>
+        </ListItem>
+      );
+    });
+  };
+
   render() {
     return (
-      <View>
+      <View style={{ height: "100%", width: "100%" }}>
         <Header>
-          <Left />
           <Body>
-            <Title>Select Workout</Title>
+            <Title>Select Muscle Groups</Title>
           </Body>
-          <Right />
         </Header>
         <ScrollView
-          style={{
-            flex: 1,
-            width: "100%",
-            paddingLeft: 24,
-            paddingRight: 24
-          }}
           contentContainerStyle={{
-            alignItems: "center"
+            alignItems: "center",
+            paddingTop: 8,
+            paddingRight: 16,
+            paddingLeft: 16,
+            width: "100%"
           }}
         >
-          <View
-            style={{
-              flex: 1,
-              flexDirection: "row",
-              justifyContent: "space-between",
-              width: "100%"
-            }}
-          >
-            <Button
-              onPress={() => {
-                this.handleSelectedExerciseButtonClick(
-                  "exercisesScreen",
-                  "Arms"
-                );
-              }}
-              iconLeft
-              block
-              primary
-              bordered
-              style={{
-                height: 100,
-                width: "48%",
-                marginTop: 8,
-                marginBottom: 8,
-                borderColor: "#1C9963"
-              }}
-            >
-              <Icon name="hand" style={{ color: "#1C9963" }} />
-              <Text style={{ fontSize: 28, color: "#1C9963" }}>arms</Text>
-            </Button>
-
-            <Button
-              onPress={() => {
-                this.handleSelectedExerciseButtonClick(
-                  "exercisesScreen",
-                  "Back"
-                );
-              }}
-              iconLeft
-              block
-              bordered
-              primary
-              style={{
-                height: 100,
-                width: "48%",
-                marginTop: 8,
-                marginBottom: 8,
-                borderColor: "#1C9963"
-              }}
-            >
-              <Icon name="logo-android" style={{ color: "#1C9963" }} />
-              <Text style={{ fontSize: 28, color: "#1C9963" }}>back</Text>
-            </Button>
-          </View>
-
-          <View
-            style={{
-              flex: 1,
-              flexDirection: "row",
-              justifyContent: "space-between",
-              width: "100%"
-            }}
-          >
-            <Button
-              onPress={() => {
-                this.handleSelectedExerciseButtonClick(
-                  "exercisesScreen",
-                  "Legs"
-                );
-              }}
-              iconLeft
-              block
-              bordered
-              primary
-              style={{
-                height: 100,
-                width: "48%",
-                marginTop: 8,
-                marginBottom: 8,
-                borderColor: "#1C9963"
-              }}
-            >
-              <Icon name="logo-snapchat" style={{ color: "#1C9963" }} />
-              <Text style={{ fontSize: 28, color: "#1C9963" }}>legs</Text>
-            </Button>
-
-            <Button
-              onPress={() => {
-                this.handleSelectedExerciseButtonClick(
-                  "exercisesScreen",
-                  "Chest"
-                );
-              }}
-              iconLeft
-              block
-              bordered
-              primary
-              style={{
-                height: 100,
-                width: "48%",
-                marginTop: 8,
-                marginBottom: 8,
-                borderColor: "#1C9963"
-              }}
-            >
-              <Icon name="contact" style={{ color: "#1C9963" }} />
-              <Text style={{ fontSize: 28, color: "#1C9963" }}>chest</Text>
-            </Button>
-          </View>
-
-          <Button
-            onPress={() => {
-              this.handleSelectedExerciseButtonClick(
-                "exercisesScreen",
-                "Upper Body"
-              );
-            }}
-            iconLeft
-            block
-            bordered
-            primary
-            style={{
-              height: 100,
-              marginTop: 8,
-              marginBottom: 8,
-              borderColor: "#1C9963"
-            }}
-          >
-            <Icon name="contrast" style={{ color: "#1C9963" }} />
-            <Text style={{ fontSize: 28, color: "#1C9963" }}>upper body</Text>
-          </Button>
-
-          <Button
-            onPress={() => {
-              this.handleSelectedExerciseButtonClick(
-                "exercisesScreen",
-                "Lower Body"
-              );
-            }}
-            iconLeft
-            block
-            bordered
-            primary
-            style={{
-              height: 100,
-              marginTop: 8,
-              marginBottom: 8,
-              borderColor: "#1C9963"
-            }}
-          >
-            <Icon name="flash" style={{ color: "#1C9963" }} />
-            <Text style={{ fontSize: 28, color: "#1C9963" }}>lower body</Text>
-          </Button>
-
-          <Button
-            onPress={() => {
-              this.handleSelectedExerciseButtonClick(
-                "exercisesScreen",
-                "Full Body"
-              );
-            }}
-            iconLeft
-            block
-            bordered
-            primary
-            style={{
-              height: 120,
-              marginTop: 8,
-              marginBottom: 8,
-              borderColor: "#1C9963"
-            }}
-          >
-            <Icon name="man" style={{ color: "#1C9963" }} />
-            <Text style={{ fontSize: 28, color: "#1C9963" }}>full body</Text>
-          </Button>
+          {this.renderSelectList(this.state.muscleGroups)}
         </ScrollView>
       </View>
     );
